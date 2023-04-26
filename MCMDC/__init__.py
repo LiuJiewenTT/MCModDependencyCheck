e205cb737bc9233be4bb3dcb8745b5014699960f
刘杰文 <1635370493@qq.com>
2023-04-26 11:29:00 +0800

    none

---
+++ MCMDC/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    'common',
+    'constants',
+    'DebugMode',
+    'DependencyInfo',
+    'docs_helper',
+    'Info',
+    'Interval',
+    'main',
+    'makeinit',
+    'Mod',
+    'ModInfo',
+    'strings',
+    'Version',
+    'VersionRule'
+]
---
+++ mcmdc_cui_main.py
@@ -0,0 +1,3 @@
+# __import__('MCMDC.main', globals=globals(), fromlist=('MCMDC',))
+from MCMDC import *
+# print(globals())
